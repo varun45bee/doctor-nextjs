e855cabc4f3474e680e9b3dc5e3821ef850a076f
varun sardar <sardarvarun121@gmail.com>
2026-07-24 22:10:59 +0530

    u

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,24 +5,15 @@ export async function GET(req: NextRequest) {
   const token = req.nextUrl.searchParams.get("hub.verify_token");
   const challenge = req.nextUrl.searchParams.get("hub.challenge");
 
-  console.log({
-    mode,
-    token,
-    challenge,
-    verifyToken: process.env.VERIFY_TOKEN,
-  });
-
-  if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+  if (
+    mode === "subscribe" &&
+    token === process.env.VERIFY_TOKEN
+  ) {
     return new Response(challenge ?? "", { status: 200 });
   }
 
   return NextResponse.json(
-    {
-      mode,
-      token,
-      verifyToken: process.env.VERIFY_TOKEN,
-      error: "Verification failed",
-    },
+    { error: "Verification failed" },
     { status: 403 }
   );
 }
