fd5d179dfb5550754efa6a2ad6f7afee7affe005
varun sardar <sardarvarun121@gmail.com>
2026-07-26 00:50:44 +0530

    u

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: ['/', '/_next/static/', '/_next/image'],
-      disallow: ['/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/_next/static/', '/_next/image'],
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: '*',
+        disallow: ['*'],
+        host: '*.vercel.app',
+      },
+    ],
     sitemap: 'https://www.pratimaagale.in/sitemap.xml',
   }
 }
